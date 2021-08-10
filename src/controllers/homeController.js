9804879d6f7480cbafcbd87c19488c790b141f8d
HoloDD <nikvk2001@gmail.com>
2021-08-10 20:39:29 +0300

    fix

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,12 @@
 const { unitScheme } = require('../models/location');
+const mongoose = require('mongoose');
 
 class homeController {
   async add(req, res, next) {
     try {
+      // const { name, className, needCrew, cost} = req.body
+      // const Unit = mongoose.model('Unit', unitScheme);
+      //TODO
     } catch (error) {}
   }
 
--- src/models/location.js
+++ src/models/location.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const unitScheme = new mongoose.Schema({
   name: { type: String, required: true },
-  className: { type: String, required: true },
+  className: { type: String, required: false },
   needCrew: Boolean,
   cost: { type: Number, required: true },
 });
